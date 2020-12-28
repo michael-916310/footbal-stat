3dab3b2546e96979fb6ae3405f4552b2fed0c555
michael-916310 <916310@rambler.ru>
2020-12-28 14:39:08 +0400

    feat: еначал компонент поиска

--- src/features/GameHeader/GameHeader.js
+++ src/features/GameHeader/GameHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SelectComponent from '../SelectComponent/SelectComponent';
+
 import './GameHeader.scss';
 
 export default function GameHeader(){
   return (
     <section>
       <div>
+        <SelectComponent></SelectComponent>
         <input type="text" className="GameHeader__parent-area" placeholder="select parent area"/>
         <input type="text" className="GameHeader__area" placeholder="select area"/>
         <input type="text" className="GameHeader__year" placeholder="select year"/>
